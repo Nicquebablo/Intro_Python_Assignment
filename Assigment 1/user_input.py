72f7b3fb305a06d86757e4eebdd6a49b530f6f91
Nicquebablo <152677936+Nicquebablo@users.noreply.github.com>
2024-02-22 10:17:56 +0300

    Add files via upload

---
+++ Assigment 1/user_input.py
@@ -0,0 +1,5 @@
+name = input( "Enter your Name: ")
+age = input ("Enter your Age ")
+location = input ("Enter you Location ")
+
+print (f"Hello {name} you are {age} years old and live in {location}.")
